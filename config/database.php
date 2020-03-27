e4c76ae7fbddad31b6413a94e9b0d652f22d6e0a
ice <190520601@qq.com>
2020-03-27 14:15:49 +0800

    修复数据库调试模式关闭仍然记录SQL日志问题

--- config/database.php
+++ config/database.php
@@ -39,7 +39,7 @@ return [
             // 数据库表前缀
             'prefix'            => Env::get('database.prefix', ''),
             // 数据库调试模式
-            'debug'             => Env::get('database.debug', true),
+            'trigger_sql'             => Env::get('database.debug', true),
             // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
             'deploy'            => 0,
             // 数据库读写是否分离 主从式有效
@@ -52,6 +52,8 @@ return [
             'fields_strict'     => true,
             // 是否需要断线重连
             'break_reconnect'   => false,
+            // 开启字段缓存
+            'fields_cache'      => false,
             // 字段缓存路径
             'schema_cache_path' => app()->getRuntimePath().'schema'.DIRECTORY_SEPARATOR,
         ],
