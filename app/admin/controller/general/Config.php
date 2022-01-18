777c75b1b1d123659dadba751b3b0aeb65d3c29b
itniu <1816739653@qq.com>
2022-01-18 10:17:54 +0800

    修复mysql版本不同编辑报错问题

--- app/admin/controller/general/Config.php
+++ app/admin/controller/general/Config.php
@@ -138,6 +138,7 @@ class Config extends Backend
                             $value = is_array($value) ? implode(',', $value) : $value;
                         }
                         $v['value'] = $value;
+                        $v['setting'] = $v['setting'] ? $v['setting'] : '';
                         $configList[] = $v->toArray();
                     }
                 }
